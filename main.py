3b6b8fe82a422d9ea1a97861ec760b4414f3fb96
Mohammed faheem <44580643+faheem77@users.noreply.github.com>
2022-10-11 15:28:25 +0500

    main file changes

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from selenium import webdriver
 import pandas as pd
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 import requests
 from bs4 import BeautifulSoup
-option = Options()
-option.add_argument("--headless")
-driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()), options=option)
+import json
 class scrap:
+    Seller_Platform='AMD-Lasers'
+    manufacture='-1'
+    manufacture_code='-1'
     titles=[]
-    skus=[]
-    descripts= []
-    images= []
+    Seller_SKU=[]
+    Categories= '-1'
+    Sub_Categories='-1'
+    Descriptions= []
+    Packaging='-1'
+    Product_Page_URL=[]
+    Image_URL= []
+    Att_url='-1'
+    Attributes='-1'
     p_url=[]
     links=[]
     p_links=[]
     def __init__(self):
         self.parse()
+        
         self.save_data()
         
     def parse(self):
@@ -27,6 +30,7 @@ class scrap:
         bs=BeautifulSoup(r.content,'lxml')
         divs= bs.findAll('a',class_='site-nav lvl-1')
         for i in divs:
+           
             link= url+i.get('href')
             if 'products/' in link:
                 self.p_links.append(link)
@@ -41,31 +45,68 @@ class scrap:
             for i in divs:
                 self.p_links.append(url+i.get('href'))
 
-        for p in self.p_links:
-            imgs=[]
-            driver.get(p)
-#             print(p)
-            title= driver.find_element(By.XPATH,"//h1[@class='product-single__title']")
-            sku= driver.find_element(By.XPATH,'//span[@class="variant-sku"]')
-            description=driver.find_element(By.XPATH,"//div[@class='product-description rte']")
-            image= driver.find_elements(By.XPATH,"//img[@class='FeaturedImage-product-template product-featured-img js-zoom-enabled']")
-            self.titles.append(title.text)
-            self.skus.append(sku.text)
-            self.p_url.append(p)
-            for img in image:
-                imgs.append(img.get_attribute('src'))
+        for p in set(self.p_links):
+            
+            
+            r= requests.get(p)
+            bs = BeautifulSoup(r.content, 'lxml')
 
-            self.images.append(imgs)
-            try:
-                self.descripts.append(description.text)
-            except:
-                self.descripts.append("No description available")
+            variantjson= bs.find('script', id='yoast-schema-graph')
+            v_json= json.loads(variantjson.text)
+            
+            for v_r in v_json['@graph'][5]['offers']:
+                imgs=[]
+                name= v_r['name']
+                self.titles.append(name)
+                sku= v_r['sku']
+                self.Seller_SKU.append(sku)
+                p_link =v_r['url']
+                self.Product_Page_URL.append(p_link)
+                r= requests.get(p_link)
+                bs= BeautifulSoup(r.content,'lxml')
+                description= bs.find('div', class_='product-description rte')
+                image= bs.findAll('img', class_='FeaturedImage-product-template product-featured-img js-zoom-enabled')
+                for img in image:
+                    imgs.append("https:"+img.get('src'))
+                self.Image_URL.append(imgs)
+                if len(description.text)>2:
+                    self.Descriptions.append(description.text)
+                if len(description.text)<1:
+                    try:
+                        descript1= bs.find('p', class_='sc-jWUzzU cKWSZG pf-33_')
+                        descript2= bs.find('div', class_='sc-llYSUQ bhKSKZ pf-37_ pf-r pf-r-eh')
+                        descript3= bs.find('p', class_='sc-jWUzzU cKWSZG pf-88_')
+#                         print('descript1',descript1.text)
+#                         print("descript2", descript2.text)
+#                         print("descript3",descript3.text)
+                        description= ''.join([descript1.text,descript2.text,descript3.text])
+                        self.Descriptions.append(description)
+#                         print(description)
+                    except:
+                        pass
+            
+        
+
+           
     def save_data(self):
-        data_dict={"Title":self.titles, "Seller SKU": self.skus, "Description":self.descripts, "Image URL": self.images, "Product Page URL":self.p_url}
+        
+#         print(len(self.titles))
+#         print(len(self.Seller_SKU))
+#         print(len(self.Product_Page_URL))
+#         print(len(self.Image_URL))
+#         print(len(self.Descriptions))
+        data_dict={"Seller Platform":self.Seller_Platform, "Seller SKU":self.Seller_SKU, "Manufacture":self.manufacture,"Manufacture Code":self.manufacture_code,
+          "Product Title":self.titles,"Description":self.Descriptions,
+         "Packaging":self.Packaging,"Categories":self.Categories,
+          "Subcategories":self.Sub_Categories,
+           "Product Page URL":self.Product_Page_URL,"Attachment URL":self.Att_url,"Image URL":self.Image_URL, "Attributes":self.Attributes }
         df= pd.DataFrame.from_dict(data_dict)
-        #droping duplicates in the dataframe
-        df.drop_duplicates()
-        df.to_csv("AMDlasers.csv", index=False)
+        df.to_csv("AMD-Lasers-data.csv", index=False)
+        
+        
 
+
+    
 if __name__ =='__main__':
     scrap=scrap()
+    scrap
